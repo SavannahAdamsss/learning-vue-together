95dc5d5aa78d0d42cf87e3c1949dadf747673c59
Casey Herold <caseynate@gmail.com>
2018-04-11 14:04:33 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Movies />
+  <!-- <div id="app">
+    <h1>{{ msg }}</h1>
+    <p v-if="isAwesome">Galvanize is AWESOME!!!</p>
+    <p v-else>Galvanize is not awesome</p>
+    <ul>
+      <li v-for="item of items" :key="item">{{ item }}</li>
+    </ul>
+    <p :class="{hideMe: isHidden}">Hide me</p>
+  </div> -->
+</template>
+
+<script>
+import Movies from './components/Movies.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Movies
+  },
+  data () {
+    return {
+      msg: 'Hello Galvanize',
+      isAwesome: false,
+      items: ['poop', 'butt', 'hello'],
+      isHidden: true
+    }
+  }
+}
+</script>
+
+<style>
+  .hideMe {
+    display: none;
+  }
+</style>
---
+++ src/components/Movies.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h1>MOVIES!!!</h1>
+		<form @submit.prevent="searchForMovies()">
+			<label for="search">Search</label>
+			<input v-model="searchTerm" type="text" name="search" id="search">
+			<button type="submit">Search for Movies</button>
+		</form>
+		<h3>{{searchTerm}}</h3>
+	</div>	
+</template>
+
+<script>
+export default {
+  data() {
+	  return {
+		  searchTerm: ''
+	  }
+  },
+  methods: {
+	  searchForMovies(){
+		  console.log(this.searchTerm)
+	  }
+  }
+}
+</script>
+
+<style>
+
+</style>
